a271c366dbafd3211cf481d93e2d6e4f1a2b8e1e
sir <sir@local>
2020-09-15 03:56:06 +0300

    init

---
+++ build/precache-manifest.9ee8cb88b2a193ed62b34f8a0b0851aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933c77a37f9c5c5d8cf5eccffadccdb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5020f4b2da93bf8f4101",
+    "url": "/static/css/2.2d6169ee.chunk.css"
+  },
+  {
+    "revision": "5020f4b2da93bf8f4101",
+    "url": "/static/js/2.a74222c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a74222c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0991984adcaf6a3bd59",
+    "url": "/static/js/main.33c8ab68.chunk.js"
+  },
+  {
+    "revision": "2f729f8dfb644b1198eb",
+    "url": "/static/js/runtime-main.4146942f.js"
+  }
+]);
